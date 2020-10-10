29c863ce57a63b5d49aeee39ae314375a92f87d0
manuel mariscal <i72marom@uco.es>
2020-10-10 19:02:29 +0200

    parte 1 - en pruebas

--- practica1/src/heapSort/heapSort.cc
+++ practica1/src/heapSort/heapSort.cc
@@ -1,8 +1,8 @@
 #include "heapSort.h"
 
 void ordenacionHeapSort() {
-	vector<double> tiemposReales;
-	vector<int> nElementos;
+	vector<double> tiemposReales, ajuste(2);
+	vector<double> nElementos, tEstimado;
 	int max, min, incremento, rep;
 
 	cout << "Número mínimo de elementos: ";
@@ -16,7 +16,15 @@ void ordenacionHeapSort() {
 
 	tiemposOrdenacionHeapSort(min, max, rep, incremento, tiemposReales, nElementos);
 
-	escribeFichero(tiemposReales, nElementos);
+	ajusteNlogN(nElementos, tiemposReales, ajuste);
+
+	calcularTiemposEstimadosNlogN(nElementos, ajuste, tEstimado);
+	
+	escribeFichero(tiemposReales, tEstimado, nElementos);
+
+	cout << "Coeficiente de determinacion: ";
+	cout << calcularCoeficienteDeterminacion(tiemposReales, tEstimado);
+	cout << endl;
 }
 
 bool heapSort(vector<int> &v) {
@@ -39,7 +47,7 @@ void rellenarVector(vector<int> &v) {
 }
 
 void tiemposOrdenacionHeapSort(int min, int max, int rep, int incremento,
-	vector<double> &tiemposReales, vector<int> &nElementos) {
+	vector<double> &tiemposReales, vector<double> &nElementos) {
 	vector<int> heap;
 	Clock Tiempo;
 	double media_tiempo = 0;
@@ -60,17 +68,89 @@ void tiemposOrdenacionHeapSort(int min, int max, int rep, int incremento,
 
 		tiemposReales.push_back(media_tiempo/rep);
 		nElementos.push_back(i);
+
+		cout << nElementos.back() << " " << tiemposReales.back() << endl;
 	}
 }
 
-void escribeFichero(vector<double> &time, vector<int> &nElements) {
-	auto it_nElements = nElements.begin();
+void escribeFichero(vector<double> &tReal, vector<double> &tEstimado,
+vector<double> &nElements) {
 	ofstream f("../text/prueba.txt");
 
-	for (auto &it_tiempos : time) {
-		f << *it_nElements << " : " << it_tiempos << endl;
-		it_nElements++;
+	for (int i = 0; i < nElements.size(); ++i) {
+		f << nElements[i] << " " << tReal[i] << " " << tEstimado[i] << endl;
 	}
 
 	f.close();
+}
+
+void ajusteNlogN(const vector <double> &n, const vector <double> &tiemposReales,
+	vector <double> &ajuste) {
+
+	// z = n*log(n): cambio de variable para facilitar el ajuste
+	vector<double> z;
+	for (auto &it : n) z.push_back(it * log(it));
+
+	// Declaración de matrices
+	// A: Matriz de coeficientes.
+	// X: Matriz de incognitas (a0 y a1).
+	// B: Matriz de terminos independientes.
+	vector <vector <double>> A(2, vector <double>(2));
+	vector <vector <double>> X(2, vector <double>(1));
+	vector <vector <double>> B(2, vector <double>(1));
+
+	// Rellenar la matriz A
+	A[0][0] = sumatorio(z, tiemposReales, 0, 0);
+	A[0][1] = sumatorio(z, tiemposReales, 1, 0);
+	A[1][0] = A[0][1];
+	A[1][1] = sumatorio(z, tiemposReales, 2, 0);
+
+	// Rellenar la matriz B.
+	B[0][0] = sumatorio(z, tiemposReales, 0, 1);
+	B[1][0] = sumatorio(z, tiemposReales, 1, 1);
+
+	// Resolución del sistema de ecuaciones.
+	resolverSistemaEcuaciones(A, B, 2, X);
+	ajuste[0] = X[0][0];
+	ajuste[1] = X[1][0];
+
+	cout << "a0 : " << ajuste[0] << endl;
+	cout << "a1 : " << ajuste[1] << endl;
+}
+
+double sumatorio(vector <double> &n, const vector <double> &t, int expN, int expT) {
+	double sumatorio = 0;
+
+	for (int i = 0; i < n.size(); ++i) 
+		sumatorio += pow(n[i], expN) * pow(t[i], expT);
+
+	return sumatorio;
+}
+
+void calcularTiemposEstimadosNlogN(const vector <double> &n,
+const vector <double> &a, vector <double> &tiemposEstimados) {
+	for (auto &i : n)
+		tiemposEstimados.push_back(a[0] + a[1] * i * log(i));
+}
+
+double calcularCoeficienteDeterminacion(const vector <double> &tiemposReales,
+const vector <double> &tiemposEstimados) {
+	double media_r = 0, media_e = 0, varianza_r = 0, varianza_e = 0;
+
+	// calculo de la media
+	for (int i = 0; i < tiemposReales.size(); ++i) {
+		media_e += tiemposEstimados[i];
+		media_r += tiemposReales[i];
+	}
+
+	media_r /= tiemposReales.size();
+	media_e /= tiemposEstimados.size();
+
+	// calculo de la varianza
+	for (int i = 0; i < tiemposReales.size(); ++i) {
+		varianza_e += pow(tiemposEstimados[i] - media_e, 2);
+		varianza_r += pow(tiemposReales[i] - media_r, 2);
+	}
+
+	return varianza_e / varianza_r;
 }
--- practica1/src/heapSort/heapSort.h
+++ practica1/src/heapSort/heapSort.h
@@ -2,7 +2,9 @@
 #define HEAPSORT_H
 
 #include "../classTime/classTime.h"
+#include "../sistemaEcuaciones/sistemaEcuaciones.hpp"
 #include <vector>
+#include <cmath>
 #include <algorithm>
 #include <cstdlib>
 #include <fstream>
@@ -43,13 +45,48 @@ bool estaOrdenado(const vector<int> &v);
  * @param nElements [description]
  */
 void tiemposOrdenacionHeapSort(int min, int max, int rep, int incremento,
-	vector<double> &realTime, vector<int> &nElements);
+	vector<double> &realTime, vector<double> &nElementos);
 
 /**
  * [escribeFichero description]
  * @param time      [description]
  * @param nElements [description]
  */
-void escribeFichero(vector<double> &time, vector<int> &nElements);
+void escribeFichero(vector<double> &tReal, vector<double> &tEstimado,
+vector<double> &nElements);
+
+/**
+ * Ajuste de la función a0 + a1*NlogN mediante el método de minimos cuadrados:
+ * @li Se hace un cambio de variable: z = NlogN
+ * @li Se ajusta una función lineal: a0 + a1*z.
+ * @param n             Vector con los tamaños para los que se han hecho las pruebas.
+ * @param tiemposReales Tiempos obtenidos en la ordenación del vector.
+ * @param ajuste        Vector de coeficientes de la curva de ajuste.
+ */
+void ajusteNlogN(const vector <double> &n, const vector <double> &tiemposReales,
+	vector <double> &ajuste);
+
+/**
+ * Sumatorio matemático.
+ * @param n     Recibe los valores de z = NlogN.
+ * @param  t    Tiempos reales.
+ * @param  expN Exponente para los valores de n.
+ * @param  expT Exponente para los valores de t.
+ * @return      Devuelve el sumatorio de n^epxN * t^expT.
+ */
+double sumatorio(vector <double> &n, const vector <double> &t, int expN, int expT);
+
+/**
+ * Teniendo en cuenta la función ajustada ajusteNlogN, se obtendrán 
+ * los tiempos estimados mediante dicha función.
+ * @param n                Tamaños de vector con los que se han hecho las pruebas iniciales.
+ * @param a                Vector de coeficientes de la curva de ajuste.
+ * @param tiemposEstimados Vector que almacena los tiempos estimados.
+ */
+void calcularTiemposEstimadosNlogN(const vector <double> &n,
+const vector <double> &a, vector <double> &tiemposEstimados);
+
+double calcularCoeficienteDeterminacion(const vector <double> &tiemposReales,
+const vector <double> &tiemposEstimados);
 
 #endif
